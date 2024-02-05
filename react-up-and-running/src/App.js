30c4d59c43fbdeef3a2de9de2de0dd49b562684a
tamlt2704 <tamlt2704@gmail.com>
2024-02-05 03:24:54 +0000

    3-react-up-and-running-adding-td-content

--- react-up-and-running/src/App.js
+++ react-up-and-running/src/App.js
@@ -3,7 +3,48 @@ import './App.css';
 
 function App() {
   const headers = ['Book', 'Author', 'Language', 'Published', 'Sales'];
-  
+  const data = [
+    [
+      'A Tale of Two Cities',
+      'Charles Dickens',
+      'English',
+      '1859',
+      '200 million',
+    ],
+    [
+      'Le Petit Prince (The Little Prince)',
+      'Antoine de Saint-Exupéry',
+      'French',
+      '1943',
+      '150 million',
+    ],
+    [
+      "Harry Potter and the Philosopher's Stone",
+      'J. K. Rowling',
+      'English',
+      '1997',
+      '120 million',
+    ],
+    [
+      'And Then There Were None',
+      'Agatha Christie',
+      'English',
+      '1939',
+      '100 million',
+    ],
+    [
+      'Dream of the Red Chamber',
+      'Cao Xueqin',
+      'Chinese',
+      '1791',
+      '100 million',
+    ],
+    ['The Hobbit', 'J. R. R. Tolkien', 'English', '1937', '100 million'],
+  ];
+
+
+  console.log(data);
+
   return (
     <table>
       <thead> 
@@ -11,6 +52,15 @@ function App() {
           {headers.map((title, idx) => <th key={idx}> {title} </th>)} 
         </tr>
       </thead>
+      <tbody>
+        {data.map((row, idx) => (
+          <tr key={idx}>
+            {row.map((cell, idx) => (
+              <td key={idx}> {cell} </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
     </table>
   );
 }
